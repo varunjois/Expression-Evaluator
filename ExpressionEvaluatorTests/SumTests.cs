8dd632434e2d80595c17ea72c7efec9af75d2fd5
Jeremy Roberts <jeremy@robertsisland.com>
2015-12-10 12:29:53 -0500

    Additional unit tests.

--- ExpressionEvaluatorTests/SumTests.cs
+++ ExpressionEvaluatorTests/SumTests.cs
@@ -15,6 +15,13 @@ namespace ExpressionEvaluatorTests
         public void clear() { func.Clear(); }
 
         [Test]
+        public void Sum_NestedSumFunctions_CorrectValue()
+        {
+            func.Function = @"Sum(1, Sum(1, 2, Sum(1,2,3)), 3)";
+            Assert.AreEqual(13, func.EvaluateNumeric());
+        }
+
+        [Test]
         public void Sum_NumbersAndLetter_LetterIgnored()
         {
             func.Function = @"Sum(a, 2, 3, 4)";
@@ -43,6 +50,15 @@ namespace ExpressionEvaluatorTests
             }
             catch {}
             func.Function = @"Sum(1) + Sum(1)";
+        }
+
+        [Test]
+        public void Sum_WithVariable001_CorrectValue()
+        {
+            func.Function = @"Sum(a, b)";
+            func.AddSetVariable("a", 1);
+            func.AddSetVariable("b", 2);
+            Assert.AreEqual(3, func.EvaluateNumeric());
         }
     }
 }
