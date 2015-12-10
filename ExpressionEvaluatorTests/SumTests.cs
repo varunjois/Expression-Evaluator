e494a85bb976254354b89eaa4fb87a5ccc376a68
Jeremy Roberts <jeremy@robertsisland.com>
2015-12-10 12:30:54 -0500

    Additional unit tests.
    From @Ic014000 (Anup Ghosh)

--- ExpressionEvaluatorTests/SumTests.cs
+++ ExpressionEvaluatorTests/SumTests.cs
@@ -22,10 +22,17 @@ namespace ExpressionEvaluatorTests
         }
 
         [Test]
-        public void Sum_NumbersAndLetter_LetterIgnored()
+        public void Sum_NumbersAndVariable_EmptyVariableIgnored()
         {
             func.Function = @"Sum(a, 2, 3, 4)";
             Assert.AreEqual(9, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_Add_IsCorrect()
+        {
+            func.Function = @"sum(5,6)+Sum(1, 2, 3, 4)";
+            Assert.AreEqual(21, func.EvaluateNumeric());
         }
 
         [Test]
@@ -33,6 +40,55 @@ namespace ExpressionEvaluatorTests
         {
             func.Function = @"Sum(1, 2, 3, 4)";
             Assert.AreEqual(10, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_Multiply_1_IsCorrect()
+        {
+            func.Function = @"sum(5,6)*Sum(1, 2, 3, 4)";
+            Assert.AreEqual(110, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_Multiply_Post_IsCorrect()
+        {
+            func.Function = @"Sum(5, 2, 3, 4)*2";
+            Assert.AreEqual(28, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_subtract_IsCorrect()
+        {
+            func.Function = @"sum(5,6)-Sum(1, 2, 3, 4)";
+            Assert.AreEqual(1, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_subtract_decimal_Devide_IsCorrect()
+        {
+            func.Function = @"(sum(6.5,6)-Sum(1.5, 2, 3, 4))/2";
+            Assert.AreEqual(1, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_subtract_decimal_IsCorrect()
+        {
+            func.Function = @"sum(5.5,6)-Sum(1.5, 2, 3, 4)";
+            Assert.AreEqual(1, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_subtract_decimal_Multiply_Both_IsCorrect()
+        {
+            func.Function = @"4*sum(5,6)";
+            Assert.AreEqual(44, func.EvaluateNumeric());
+        }
+
+        [Test]
+        public void Sum_Numbers_subtract_decimal_Multiply_IsCorrect()
+        {
+            func.Function = @"(sum(6.5,6)-Sum(1.5, 2, 3, 4))*2";
+            Assert.AreEqual(4, func.EvaluateNumeric());
         }
 
         [Test]
