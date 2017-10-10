02a1cdf5e1cc135bd8627e95144869d931eb4b52
Varun LV <varun.lv@siemens.com>
2017-10-10 14:44:10 +0530

    Unit test implementation for string case verification

--- ExpressionEvaluatorTests/ToStringFunctionTests.cs
+++ ExpressionEvaluatorTests/ToStringFunctionTests.cs
@@ -38,6 +38,54 @@ namespace ExpressionEvaluatorTests
         }
 
         [Test]
+        public void ToStringOperator_FalseConditionEvaluated_OperandCaseMatch()
+        {
+            func.Function = "toString(if(a>1){'Test'}else{'NotValid'})";
+            func.AddSetVariable("a", 1);
+            Assert.AreEqual("NotValid", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_FalseConditionEvaluated_OperatorIgnoreCase()
+        {
+            func.Function = "toString(If(a>1){'Test'}else{'NotValid'})";
+            func.AddSetVariable("a", 1);
+            Assert.AreEqual("NotValid", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_LHSCamelWithoutSpace_FalseConditionEvaluated()
+        {
+            func.Function = "toString(If(a == 'hi '){'hi with space'}else{'NotValid'})";
+            func.AddSetVariable("a", "Hi");
+            Assert.AreEqual("NotValid", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_LHSCamelWithSpace_TrueConditionEvaluated()
+        {
+            func.Function = "toString(If(a == 'hi '){'Hi with space'}else{'NotValid'})";
+            func.AddSetVariable("a", "Hi ");
+            Assert.AreEqual("Hi with space", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_LHSLowerWithoutSpace_FalseConditionEvaluated()
+        {
+            func.Function = "toString(If(a == 'hi '){'hi with space'}else{'NotValid'})";
+            func.AddSetVariable("a", "hi");
+            Assert.AreEqual("NotValid", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_LHSLowerWithSpace_TrueConditionEvaluated()
+        {
+            func.Function = "toString(If(a == 'hi '){'hi with space'}else{'NotValid'})";
+            func.AddSetVariable("a", "hi ");
+            Assert.AreEqual("hi with space", func.Evaluate<string>());
+        }
+
+        [Test]
         public void ToStringOperator_NumericInput_IsCorrect()
         {
             func.Function = "toString(2)";
@@ -63,6 +111,29 @@ namespace ExpressionEvaluatorTests
         {
             func.Function = "toString( 'test' )";
             Assert.AreEqual("test", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_TrueConditionEvaluated_OperandCaseMatch()
+        {
+            func.Function = "toString(if(a>1){'Test'}else{'NotValid'})";
+            func.AddSetVariable("a", 2);
+            Assert.AreEqual("Test", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_TrueConditionEvaluated_OperatorIgnoreCase()
+        {
+            func.Function = "toString(If(a>1){'Test'}else{'NotValid'})";
+            func.AddSetVariable("a", 2);
+            Assert.AreEqual("Test", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_VariableCaseMatch_CaseMismatchError()
+        {
+            func.Function = "toString('Test')";
+            Assert.AreNotEqual("test", func.Evaluate<string>());
         }
 
         [Test]
@@ -103,6 +174,30 @@ namespace ExpressionEvaluatorTests
             func.Function = "toString(a)";
             func.AddSetVariable("a", "test");
             Assert.AreEqual("test", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_VariableIsStringCaseMatch_IsCorrect()
+        {
+            func.Function = "toString(a)";
+            func.AddSetVariable("a", "Test");
+            Assert.AreEqual("Test", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_VariableWithSapce_ErrorExpectedSpaceTrimmed()
+        {
+            func.Function = "toString(a)";
+            func.AddSetVariable("a", "Test ");
+            Assert.AreNotEqual("Test", func.Evaluate<string>());
+        }
+
+        [Test]
+        public void ToStringOperator_VariableWithSapce_SpaceIsIncluded()
+        {
+            func.Function = "toString(a)";
+            func.AddSetVariable("a", "Test ");
+            Assert.AreEqual("Test ", func.Evaluate<string>());
         }
     }
 }
